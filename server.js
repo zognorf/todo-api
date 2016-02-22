844a2963fac5cb920d3a9a71f8e33ed7e3b1dc02
Matthew Froggatt <froggatt.me@gmail.com>
2016-02-21 21:01:52 -0500

    add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -58,19 +59,28 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) ||
-    body.description.trim().length === 0) {
-    return res.status(400).send();
-  }
+  db.todo.create(body)
+    .then(function(todo) {
+      res.json(todo.toJSON());
+    }, function(e) {
+      res.status(400).json(e);
+    });
 
-  // trim description (spaces at start or end, etc, are removed)
-  body.description = body.description.trim();
-  // add id field
-  body.id = todoNextId++;
-  //push body into array
-  todos.push(body);
+  /*
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) ||
+      body.description.trim().length === 0) {
+      return res.status(400).send();
+    }
 
-  res.json(body);
+    // trim description (spaces at start or end, etc, are removed)
+    body.description = body.description.trim();
+    // add id field
+    body.id = todoNextId++;
+    //push body into array
+    todos.push(body);
+
+    res.json(body);
+  */
 });
 
 // DELETE /todos/:id
@@ -120,7 +130,9 @@ app.put('/todos/:id', function(req, res) {
   res.json(matchedToDo);
 });
 
-
-app.listen(PORT, function() {
-  console.log('Express listening on port ' + PORT);
-});
+db.sequelize.sync()
+  .then(function() {
+    app.listen(PORT, function() {
+      console.log('Express listening on port ' + PORT);
+    });
+  });
