d25003555949a4f52269d9850677a55562069b64
Matthew Froggatt <froggatt.me@gmail.com>
2016-02-22 08:32:51 -0500

    convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,29 +16,31 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
-  var queryParams = req.query;
-  var filteredTodos = todos;
+  var query = req.query;
+  var where = {};
 
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed ===
-    'true') {
-    filteredTodos = _.where(filteredTodos, {
-      completed: true
-    });
-  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed ===
+  if (query.hasOwnProperty('completed') && query.completed === 'true') {
+    where.completed = true;
+  } else if (query.hasOwnProperty('completed') && query.completed ===
     'false') {
-    filteredTodos = _.where(filteredTodos, {
-      completed: false
-    });
+    where.completed = false;
   }
 
-  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    filteredTodos = _.filter(filteredTodos, function(todo) {
-      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) >
-        -1;
-    });
+  if (query.hasOwnProperty('q') && query.q.length > 0) {
+    where.description = {
+      $like: '%' + query.q + '%'
+    };
   }
 
-  res.json(filteredTodos);
+  db.todo.findAll({
+      where: where
+    })
+    .then(function(todos) {
+        res.json(todos);
+      },
+      function(e) {
+        res.status(500).send(e);
+      });
 });
 
 // GET /todos/:id
@@ -55,17 +57,6 @@ app.get('/todos/:id', function(req, res) {
     }, function(e) {
       res.status(500).send();
     });
-
-
-  /*  var matchedToDo = _.findWhere(todos, {
-      id: todoid
-    });
-
-    if (matchedToDo) {
-      res.json(matchedToDo);
-    } else {
-      res.status(404).send();
-    }*/
 });
 
 // POST /todos
