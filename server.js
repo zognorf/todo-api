a81776d076f86f8c2a49060891386744713147c3
Matthew Froggatt <froggatt.me@gmail.com>
2016-02-24 07:33:54 -0500

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -108,31 +108,32 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
   var todoid = parseInt(req.params.id, 10);
-  var matchedToDo = _.findWhere(todos, {
-    id: todoid
-  });
   var body = _.pick(req.body, 'description', 'completed');
-  var validAttributes = {};
+  var attributes = {};
 
-  if (!matchedToDo) {
-    return res.status(404).send();
+  if (body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
 
-  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    return res.status(400).send();
+  if (body.hasOwnProperty('description')) {
+    attributes.description = body.description;
   }
 
-  if (body.hasOwnProperty('description') && _.isString(body.description) &&
-    body.description.trim().length > 0) {
-    validAttributes.description = body.description;
-  } else if (body.hasOwnProperty('description')) {
-    return res.status(400).send();
-  }
-
-  _.extend(matchedToDo, validAttributes);
-  res.json(matchedToDo);
+  db.todo.findById(todoid)
+    .then(function(todo) {
+      if (todo) {
+        todo.update(attributes)
+          .then(function(todo) {
+            res.json(todo.toJSON());
+          }, function(e) {
+            res.status(400).json(e);
+          });
+      } else {
+        res.status(404).send();
+      }
+    }, function() {
+      res.status(500).send();
+    });
 });
 
 db.sequelize.sync()
