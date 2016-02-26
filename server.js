0c2b0e6667b5fb4082518ef80ee0ac8d7c948006
Matthew Froggatt <froggatt.me@gmail.com>
2016-02-26 07:55:18 -0500

    Add password hash to user model and updated POST /users

--- server.js
+++ server.js
@@ -142,13 +142,15 @@ app.post('/user', function(req, res) {
 
   db.user.create(body)
     .then(function(user) {
-      res.json(user.toJSON());
+      res.json(user.toPublicJSON());
     }, function(e) {
       res.status(400).json(e);
     });
 });
 
-db.sequelize.sync()
+db.sequelize.sync({
+    force: true
+  })
   .then(function() {
     app.listen(PORT, function() {
       console.log('Express listening on port ' + PORT);
